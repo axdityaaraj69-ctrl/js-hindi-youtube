8540db2b6bb69587d516dffaaea79788d9e8c09d
axdityaaraj69-ctrl <axdityaaraj69@gmail.com>
2026-07-09 20:22:00 +0000

    datatypes-summary

---
+++ .devcontainer/test.js/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefiened, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedin = false
+const otsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol ('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 314856749484518n
+
+
+
+// Refrence (Non primitive)
+
+// Array, Objects, Funtions
+
+const heros =[" raj", "ahona"]
+{
+    name: "raj"
+    age: 21
+}
+    
+const myFunction =  function() {
+    console.log ("Hello world")
+}
+
+console.log(typeof outsideTemp);
