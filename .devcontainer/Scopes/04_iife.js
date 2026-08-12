02e014682694baaa6c256ea5713181a3976a0646
axdityaaraj69-ctrl <axdityaaraj69@gmail.com>
2026-08-12 15:34:51 +0000

    arrow

---
+++ .devcontainer/Scopes/04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Funtion Expression (IIFE)
+
+(function chai ()
+{
+    console.log('DB CONNECTED');
+
+} ) ();
+
+
+( () =>  {
+    console.log('DB CONNECTED TWO ${name}');
+} ) ('raj') 
+
+
+
+                              
+
